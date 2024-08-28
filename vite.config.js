7fa1ea9053c1a8f5072c82f7eae852bd453ed8c1
KennethE98 <keneth.escobar@espoch.edu.ec>
2024-08-28 12:58:57 -0500

    add pwa and manifest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa'
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: 'auto',
+      includeAssets: ["vite.svg"],
+      manifest: {
+        name: 'PWA REACT AND VITE',
+        short_name: 'REACT APP',
+        description: 'React and Vite app + PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './public/vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+        ]
+      }
+    }),
+  ],
+});
